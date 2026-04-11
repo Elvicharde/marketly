bbe54c64d694b54ad313b2630d28b7ecefe26b26
elvicharde <vicharde@gmail.com>
2026-04-11 22:04:43 +0100

    fix: fixed translation of the switch button

--- src/components/ui/Switch.tsx
+++ src/components/ui/Switch.tsx
@@ -11,7 +11,7 @@ export function Switch({
       className={`w-10 h-5 rounded-full transition-colors duration-300 ${enabled ? "bg-[#0F172A]" : "bg-gray-300"} cursor-pointer`}
     >
       <span
-        className={`block w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${enabled ? "translate-x-5.5" : "translate-x-1"}`}
+        className={`block w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${enabled ? "translate-x-5.5" : "translate-x-0.5"}`}
       />
     </button>
   );
